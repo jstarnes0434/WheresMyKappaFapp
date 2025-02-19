a48efe3a9923910e08344249221c08cf3210c897
Joe Starnes <jstarnes0434@gmail.com>
2025-02-19 09:51:16 -0600

    CORS update

--- WheresMyKappaFapp/Function1.cs
+++ WheresMyKappaFapp/Function1.cs
@@ -1,6 +1,8 @@
+using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
 namespace WheresMyKappaFapp
@@ -15,10 +17,26 @@ namespace WheresMyKappaFapp
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<HttpResponseData> Run(
+    [HttpTrigger(AuthorizationLevel.Function, "get", "post", "options")] HttpRequestData req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            // Handle OPTIONS request (Preflight)
+            if (req.Method == "OPTIONS")
+            {
+                response.Headers.Add("Access-Control-Allow-Origin", "*"); // Change "*" to "http://localhost:5173" for more security
+                response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+                return response;
+            }
+
+            response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:5173"); // Adjust for production later
+            response.Headers.Add("Content-Type", "application/json");
+
+            await response.WriteStringAsync("{\"message\": \"Hello from Azure Functions!\"}");
+
+            return response;
         }
     }
 }
